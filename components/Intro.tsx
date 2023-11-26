8d525ebfb37965b3a942da3d1341cba23349b008
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-27 12:02:46 +1300

    animate components

---
+++ components/Intro.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+const Intro = () => {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <Image
+              src="/profilePic.jpg"
+              alt="Ryan profile pic"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-3xl absolute bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Intro
