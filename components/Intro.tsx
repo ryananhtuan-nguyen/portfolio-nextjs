b1ceb629f84955f595d8836156a699bd918957f2
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 12:27:57 +1300

    done intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,11 +1,15 @@
 'use client'
 
-import Image from 'next/image'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link'
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
+import { FaGithubSquare } from 'react-icons/fa'
+import { HiDownload } from 'react-icons/hi'
 
 const Intro = () => {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -38,6 +42,61 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      >
+        <span className="font-bold">Hello, I&apos;m Ryan Nguyen. </span>
+        I&apos;m a <span className="font-bold">full-stack developer </span>
+        with <span className="font-bold">experiences. </span> I enjoy building{' '}
+        <span className="italic">sites & apps</span>. My focus is{' '}
+        <span className="underline">
+          React.js, Next.js, Express.js, Typescript, databases, ...
+        </span>
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-60 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          href="Ryan_Resume.pdf"
+          download
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition " />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/ryan-nguyen-902196293/"
+          target="_blank"
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href="https://github.com/ryananhtuan-nguyen"
+          target="_blank"
+          className="bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   )
 }
