cb7285ba0f74d92f0231ffe8277f69169162d25d
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 12:33:17 +1300

    animation

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -1,8 +1,16 @@
+'use client'
 import React from 'react'
-
+import { motion } from 'framer-motion'
 const SectionDivider = () => {
   return (
-    <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block" />
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.125,
+      }}
+      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+    />
   )
 }
 
