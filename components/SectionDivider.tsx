e75553846cf9fadc16fa183c8c85b30fbc9baee8
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 12:30:53 +1300

    decoration line

---
+++ components/SectionDivider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionDivider = () => {
+  return (
+    <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block" />
+  )
+}
+
+export default SectionDivider
