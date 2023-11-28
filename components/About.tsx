ba11b7c297c81fac54a6f73643ac1fb9473abf12
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 16:12:26 +1300

    added id

--- components/About.tsx
+++ components/About.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion'
 const About = () => {
   return (
     <motion.section
+      id="about"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
