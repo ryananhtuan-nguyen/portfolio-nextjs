89c5659f0420c0fc520c9f2f5d9a520c3c583fe0
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 18:39:43 +1300

    useRef for active section

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,25 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import SectionHeading from './SectionHeading'
 import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useActiveSectionContext } from '@/context/active-section-context'
 
 const About = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.75,
+  })
+  const { setActiveSection } = useActiveSectionContext()
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection('About')
+    }
+  }, [inView, setActiveSection])
+
   return (
     <motion.section
+      ref={ref}
       id="about"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
