0ee64b2473cfc4f27557a2fb6f3ce4a47970ee51
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 19:21:26 +1300

    tracking last click

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -13,6 +13,8 @@ import type { SectionName } from '@/lib/data'
 type ActiveSectionContextType = {
   activeSection: SectionName
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+  timeOfLastClick: number
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>
 }
 
 export const ActiveSectionContext =
@@ -20,9 +22,17 @@ export const ActiveSectionContext =
 
 const ActiveSectionContextProvider = ({ children }: PropsWithChildren) => {
   const [activeSection, setActiveSection] = useState<SectionName>('Home')
+  const [timeOfLastClick, setTimeOfLastClick] = useState<number>(0)
 
   return (
-    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
       {children}
     </ActiveSectionContext.Provider>
   )
