16b06cae01760caec35a4322c1aee62af80bc954
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 14:10:44 +1300

    added experiences and project datas

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,12 @@ import React from 'react'
 import { CgWorkAlt } from 'react-icons/cg'
 import { FaReact } from 'react-icons/fa'
 import { LuGraduationCap } from 'react-icons/lu'
-import corpcommentImg from '@/public/corpcomment.png'
-import rmtdevImg from '@/public/rmtdev.png'
-import wordanalyticsImg from '@/public/wordanalytics.png'
+import wardrobeWonder from '/wardrobe-wonders.jpg'
+import messengerClone from '/messenger-clone.jpg'
+import cypress from '/cypress.png'
+import quill from '/quill.jpg'
+import digitalHippo from '/digitalhippo.jpg'
+import aiCompanion from '/ai-companion.jpg'
 
 export const links = [
   {
@@ -35,52 +38,127 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: 'Graduated bootcamp',
-    location: 'Miami, FL',
+    title: 'Freelancer, content creator, contractor',
+    location: 'New Market, Auckland',
     description:
-      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+      'Moving on from the course from Dev Academy, I immersed myself into learning and exploring new Technologies, building and deploying quite a few full-stack projects.',
     icon: React.createElement(LuGraduationCap),
-    date: '2019',
+    date: '2023 - Present',
   },
   {
     title: 'Front-End Developer',
-    location: 'Orlando, FL',
+    location: 'New Market, Auckland',
     description:
-      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+      'I graduated from Level 6 in Applied Software Development course from Dev Academy Aotearoa.',
     icon: React.createElement(CgWorkAlt),
-    date: '2019 - 2021',
+    date: '2023',
   },
   {
-    title: 'Full-Stack Developer',
-    location: 'Houston, TX',
+    title: 'Sous Chef',
+    location: 'Mission Bay, Auckland',
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      'Exceptional leadership skills honed through orchestrating kitchen operations and managing teams. Strong ability to delegate tasks efficiently while maintaining a clear vision of culinary goals. Profound teamwork experience in high-pressure kitchen environments, fostering cooperation, communication, and problem-solving. Adaptability, meticulous attention to detail, and a commitment to delivering exceptional results.',
     icon: React.createElement(FaReact),
-    date: '2021 - present',
+    date: '2014-2023',
   },
 ] as const
 
 export const projectsData = [
   {
-    title: 'CorpComment',
+    title: 'Cypress',
     description:
-      'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
-    tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
-    imageUrl: corpcommentImg,
+      'A Saas notion app where you can create and manage your own workspace, folders, and file, as well as collaborate with others with realtime update, realtime cursor, realtime selection, and realtime create/update/delete folder, file, and workspaces.',
+    tags: [
+      'React',
+      'Next.js',
+      'Supabase',
+      'Tailwind',
+      'DrizzleORM',
+      'PostgresSQL',
+      'socket.io',
+      'QuillEditor',
+      'Stripe',
+    ],
+    imageUrl: cypress,
   },
   {
-    title: 'rmtDev',
+    title: 'Wardrobe wonders',
     description:
-      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
-    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
+      'A digital wardrobe that helps you manage your own wardrobe and also can share your outfit of the day, as well as like and commenting on other users outfits. This project is our final group project built during the bootcamp in Dev Academy Aotearoa.',
+    tags: ['React', 'PostgresSQL', 'Tailwind', 'Knex.js', 'Express.js'],
+    imageUrl: wardrobeWonder,
   },
   {
-    title: 'Word Analytics',
+    title: 'Facebook messenger clone',
     description:
-      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
-    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
+      'A simple messenger app inspired by facebook messenger, where user can send messages, photos, and also see the status of the other user all in real-time',
+    tags: [
+      'React',
+      'TypeScript',
+      'Next.js',
+      'Tailwind',
+      'MongoDB',
+      'Pusher',
+      'Prisma',
+      'Axios',
+      'Cloudinary',
+      'Auth0',
+    ],
+    imageUrl: messengerClone,
+  },
+  {
+    title: 'Quillbot',
+    description:
+      'A SaaS app that give you the power to upload your own pdf, view it in your own way, and also interact with the pdf file with the help of Open AI',
+    tags: [
+      'React.js',
+      'Next.js',
+      'mysql',
+      'OpenAI',
+      'Stripe',
+      'Prisma',
+      'Pinecone',
+      'Langchain',
+      'Zod',
+      'tRPC',
+      'Tailwind',
+      'ClerkAuth',
+    ],
+    imageUrl: quill,
+  },
+  {
+    title: 'DigitalHippo',
+    description:
+      'A modern digital market place where you can sell or purchase your digital assets, with Admin dashboard provided',
+    tags: [
+      'React.js',
+      'Next.js',
+      'Express.js',
+      'MongoDB',
+      'tRPC',
+      'Payload',
+      'Resend',
+      'Swiper',
+      'Tailwind',
+    ],
+    imageUrl: digitalHippo,
+  },
+  {
+    title: 'AI-Companion',
+    description:
+      'A SaaS app where you can create your own custom AI companion and be able to interact with it.',
+    tags: [
+      'Next.js',
+      'React.js',
+      'ClerkAuth',
+      'Prisma',
+      'mysql',
+      'Tailwind',
+      'Stripe',
+      'Pinecone',
+      'OpenAI',
+    ],
+    imageUrl: aiCompanion,
   },
 ] as const
 
@@ -95,13 +173,16 @@ export const skillsData = [
   'Git',
   'Tailwind',
   'Prisma',
+  'Knex.js',
+  'DrizzleORM',
   'MongoDB',
-  'Redux',
-  'GraphQL',
+  '@tanstack/react-query',
   'Apollo',
-  'Express',
+  'Express.js',
   'PostgreSQL',
-  'Python',
-  'Django',
+  'mysql',
+  'Express.js',
   'Framer Motion',
+  'websocket',
+  'socket.io',
 ] as const
