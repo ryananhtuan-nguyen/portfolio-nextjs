121cc066dc2f7b0bf9c51487896541981d86d529
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 19:39:04 +1300

    refactor

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,8 +3,13 @@ import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import type { SectionName } from './data'
 
-export function useSectionInView(sectionName: SectionName) {
-  const { ref, inView } = useInView()
+export function useSectionInView(
+  sectionName: SectionName,
+  threshold: number = 0.75
+) {
+  const { ref, inView } = useInView({
+    threshold: threshold,
+  })
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
 
   useEffect(() => {
