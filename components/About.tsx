5eb2289debefe17fa0f5cf08d61607ee1de5ab3a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 16:18:24 +1300

    id

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3 ">
