2c07b7b3144b5e3bf38d954f8c17ea603272480a
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 14:10:54 +1300

    project img

---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion'
+import SectionHeading from './SectionHeading'
+
+const Projects = () => {
+  return (
+    <motion.section>
+      <SectionHeading>My projects</SectionHeading>
+      <div></div>
+    </motion.section>
+  )
+}
+
+export default Projects
