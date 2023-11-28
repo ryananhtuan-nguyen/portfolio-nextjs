d2154efe99e183578f0fb86a0494c8199388f348
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-28 15:04:46 +1300

    centered text

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,7 +1,11 @@
 import React, { PropsWithChildren } from 'react'
 
 const SectionHeading = ({ children }: PropsWithChildren) => {
-  return <h2 className="text-3xl font-medium capitalize mb-8">{children}</h2>
+  return (
+    <h2 className="text-3xl font-medium capitalize mb-8 text-center">
+      {children}
+    </h2>
+  )
 }
 
 export default SectionHeading
