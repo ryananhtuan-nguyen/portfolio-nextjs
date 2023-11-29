d93bd31295b5b058f77f43a50fa2919b79b4b275
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 21:23:23 +1300

    simple footer

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 text-xs">
+        &copy; 2023 Ryan Nguyen. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website:</span> Built with
+        React, Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email, Vercel hosting.
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
