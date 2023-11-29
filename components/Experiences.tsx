31b4d41c49191d769a9d743139919022eedd9e92
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 23:18:54 +1300

    darkmode css

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -9,9 +9,11 @@ import 'react-vertical-timeline-component/style.min.css'
 import { experiencesData } from '@/lib/data'
 import { useSectionInView } from '@/lib/hooks'
 import SectionHeading from './SectionHeading'
+import { useTheme } from 'next-themes'
 
 export default function Experience() {
   const { ref } = useSectionInView('Experience')
+  const { theme } = useTheme()
 
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
@@ -21,21 +23,30 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: '#f3f4f6',
+                background:
+                  theme === 'light' ? '#f3f4f6' : 'rgba(255,255,255,0.05)',
                 boxShadow: 'none',
                 border: '1px solid rgba(0, 0, 0, 0.05)',
                 textAlign: 'left',
                 padding: '1.3rem 2rem',
               }}
               contentArrowStyle={{
-                borderRight: '0.4rem solid #9ca3af',
+                borderRight:
+                  theme === 'light'
+                    ? '0.4rem solid #9ca3af'
+                    : '0.4rem solid rgba(255,255,255,0.05)',
+                fontSize: '1.5rem',
               }}
               date={item.date}
               icon={item.icon}
+              iconStyle={{
+                background:
+                  theme === 'light' ? 'white' : 'rgba(255,255,255,0.05)',
+              }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
               <p className="font-normal !mt-0">{item.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.description}
               </p>
             </VerticalTimelineElement>
