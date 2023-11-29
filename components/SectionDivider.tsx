69e88ac1714d4b05ce8e5fdf664fdd0cadd2220b
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 23:19:09 +1300

    darkmode css

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -9,7 +9,7 @@ const SectionDivider = () => {
       transition={{
         delay: 0.125,
       }}
-      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block"
+      className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-20"
     />
   )
 }
