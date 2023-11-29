eccd16ca5a98aecd057c5ea0edd97a5df5b10c36
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 22:35:40 +1300

    darkmode color for button

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -40,7 +40,7 @@ const ModeToggle = () => {
   return (
     <button
       onClick={toggleTheme}
-      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
     >
       {theme === 'light' && <BsSun />}
       {theme === 'dark' && <BsMoon />}
