4c3153a9d8362171e579e13893e21a1cc104fc01
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-30 00:21:31 +1300

    fixing aboutme

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -57,9 +57,9 @@ const Intro = () => {
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
       >
         <span className="font-bold">Hello, I&apos;m Ryan Nguyen. </span>
-        I&apos;m a <span className="font-bold">full-stack developer </span>
-        with <span className="font-bold">experiences. </span> I enjoy building{' '}
-        <span className="italic">sites & apps</span>. My focus is{' '}
+        I&apos;m a{' '}
+        <span className="font-bold">experienced full-stack developer. </span>I
+        enjoy building <span className="italic">sites & apps</span>. My focus is{' '}
         <span className="underline">
           React.js, Next.js, Express.js, Typescript, databases,
         </span>
