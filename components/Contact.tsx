0514f702d83588a0beb771d6f917e61c4976aa31
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 21:19:07 +1300

    added loading and disabled state

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -87,11 +87,17 @@ const Contact = () => {
         />
         <button
           type="submit"
-          className="group h-[3rem] w-[8rem] text-white bg-gray-900 rounded-full outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
+          className="group h-[3rem] w-[8rem] text-white bg-gray-900 rounded-full outline-none transition-all flex items-center justify-center gap-2 focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950 disabled:scale-100 disabled:bg-opacity-65"
           disabled={isSubmitting}
         >
-          Submit{' '}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+          {isSubmitting ? (
+            <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white" />
+          ) : (
+            <>
+              Submit{' '}
+              <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
+            </>
+          )}
         </button>
       </form>
     </motion.section>
