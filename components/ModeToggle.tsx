9873c419d4f1ecf82dd2a00c04e69aac059fa533
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-11-29 21:26:16 +1300

    added mode toggle

---
+++ components/ModeToggle.tsx
@@ -0,0 +1,11 @@
+import { BsSun } from 'react-icons/bs'
+
+const ModeToggle = () => {
+  return (
+    <button className="fixed bottom-5 right-5">
+      <BsSun />
+    </button>
+  )
+}
+
+export default ModeToggle
