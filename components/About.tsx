4caf4776dfc7eb05a1bd9809513b6e7ed810d86f
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-09 19:05:26 +1300

    chore: edit text

--- components/About.tsx
+++ components/About.tsx
@@ -13,28 +13,43 @@ const About = () => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-[45rem] text-left leading-8 sm:mb-40 scroll-mt-28"
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3 ">
         I&apos;m Ryan Nguyen, a passionate{' '}
         <span className="font-medium">full-stack developer </span>who thrived in
         a<span className="font-medium">rigorous 15 -week bootcamp</span> at{' '}
-        <span className="text-italic">Dev Academy</span>. With a background of
-        almost a decade as a Sous Chef, I bring leadership, teamwork, and
-        attention to detail to my new career.
+        <span className="text-italic">Dev Academy</span>.
       </p>
-      <p>
+      <p className="mb-3 ">
+        With a background of{' '}
+        <span className="font-medium">almost a decade</span> as a Sous Chef, I
+        have equipped myself with:
+        <ul className="!list-disc pl-2 sm:pl-12">
+          <li>Exceptional leadership skills</li>
+          <li>Strong ability to delegate tasks efficiently</li>
+          <li>
+            Profound teamwork experience in high-pressured environments,
+            fostering cooperation, communication and problem-solving
+          </li>
+          <li>Adaptability, meticulous attention to details</li>
+          <li>Commitment to delevering exceptional results.</li>
+        </ul>
+      </p>
+      <p className="mb-3 ">
         My core stack is{' '}
         <span className="font-medium">
-          React.js, Next.js, Node.js, MongoDB and Mysql.
-        </span>
+          React.js, TypeScript,Next.js, Node.js, MongoDB and Mysql.
+        </span>{' '}
         I am also familiar with{' '}
         <span className="font-medium text-italic">
-          TypeScript, Prisma, Drizzle ORM, Socket.io, TailwindCSS and Express.js
-          and much more.
-        </span>
-        I enjoy learning new things, watching movies, playing guitar and coding
+          Prisma, Drizzle ORM, Socket.io, TailwindCSS Express.js, and I&apos;m
+          learning more and more everyday.
+        </span>{' '}
+      </p>
+      <p>
+        I enjoy exploring new things, watching movies, playing guitar and coding
         in my free time.
       </p>
     </motion.section>
