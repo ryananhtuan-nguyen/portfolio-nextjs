c5b63c3d38f0b63eecac610e5e7cbf8e96f9b0f2
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-28 14:20:42 +1300

    added link to projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,10 +1,11 @@
-'use client'
+'use client';
 
-import { ProjectProps } from '@/lib/data'
-import { motion, useScroll, useTransform } from 'framer-motion'
-import Image from 'next/image'
-import { useRef } from 'react'
-import { useRouter } from 'next/navigation'
+import { ProjectProps } from '@/lib/data';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import Image from 'next/image';
+import { useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Project({
   title,
@@ -13,14 +14,14 @@ export default function Project({
   imageUrl,
   link,
 }: ProjectProps) {
-  const ref = useRef<HTMLDivElement>(null)
-  const router = useRouter()
+  const ref = useRef<HTMLDivElement>(null);
+  const router = useRouter();
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ['0 1', '1.33 1'],
-  })
-  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1])
-  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1])
+  });
+  const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <motion.div
@@ -31,32 +32,30 @@ export default function Project({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-x-hidden overflow-y-scroll sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-            {description}{' '}
-            <a href={link} target="_blank">
-              <span className="text-sky-500">Try it live !</span>
-            </a>
-          </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto overflow-y-scroll">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <Link href={link} target="_blank">
+        <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-x-hidden overflow-y-scroll sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+          <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
+            <h3 className="text-2xl font-semibold">{title}</h3>
+            <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70 pb-2">
+              {description}{' '}
+            </p>
+            <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto overflow-y-scroll">
+              {tags.map((tag, index) => (
+                <li
+                  className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                  key={index}
+                >
+                  {tag}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <Image
-          src={imageUrl}
-          alt="Project I worked on"
-          quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          <Image
+            src={imageUrl}
+            alt="Project I worked on"
+            quality={95}
+            className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
         transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
@@ -68,8 +67,9 @@ export default function Project({
         group-even:group-hover:rotate-2
 
         group-even:right-[initial] group-even:-left-40"
-        />
-      </section>
+          />
+        </section>
+      </Link>
     </motion.div>
-  )
+  );
 }
